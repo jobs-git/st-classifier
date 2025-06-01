534ac11f4aec4e2a2a58dd6c9bcd8e7bd3ea735a
James Guana <jdpguana@histark.com>
2025-06-01 22:37:19 +0800

    fist commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from sentence_transformers import SentenceTransformer
+
+data = pd.read_csv("customer.csv")
+
+embed_model = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2", device="cuda")
+
+X = embed_model.encode(data["utterance"].tolist()) 
+y = data["category"]  
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+log_reg = LogisticRegression()
+log_reg.fit(X_train, y_train)
+y_pred_log = log_reg.predict(X_test)
+
+print("Logistic Regression Performance:")
+print(classification_report(y_test, y_pred_log))
+
+rf_model = RandomForestClassifier()
+rf_model.fit(X_train, y_train)
+y_pred_rf = rf_model.predict(X_test)
+
+print("Random Forest Performance:")
+print(classification_report(y_test, y_pred_rf))
